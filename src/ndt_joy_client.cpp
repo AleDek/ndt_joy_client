27d5e8f803c020c34a03f59df1902a9893beb009
aledek <alessandro.decrescenzo1@gmail.com>
2024-02-21 10:54:01 +0100

    working wall control and x trajectory following

--- src/ndt_joy_client.cpp
+++ src/ndt_joy_client.cpp
@@ -21,6 +21,64 @@ SIMPLE_CLIENT::SIMPLE_CLIENT() {
     if( !_nh.getParam("joy_ax3", joy_ax3)) {
         joy_ax3 = 1.0;
     }
+
+    if( !_nh.getParam("wall_max_yaw", _wall_max_yaw)) {
+        _wall_max_yaw = M_PI/2.0;
+    }
+
+    if( !_nh.getParam("wall_max_dist", _wall_max_dist)) {
+        _wall_max_dist = 3.0;
+    }
+
+    if( !_nh.getParam("d_safe", _d_safe)) {
+        _d_safe = 1.0;
+    }
+
+    if( !_nh.getParam("delta_approch", _delta_approch)) {
+        _delta_approch = 0.3;
+    }
+
+    if( !_nh.getParam("Kp_x", _Kp_x)) {
+        _Kp_x = 0.2;
+    }
+
+    if( !_nh.getParam("Kp_yaw", _Kp_yaw)) {
+        _Kp_yaw = 0.3;
+    }
+
+    // if( !_nh.getParam("Kd_x", _Kd_x)) {
+    //     _Kd_x = 0.0;
+    // }
+
+    // if( !_nh.getParam("Kd_yaw", _Kd_yaw)) {
+    //     _Kd_yaw = 0.08;
+    // }
+
+    if( !_nh.getParam("eps_x", _eps_x)) {
+        _eps_x = 0.001;
+    }
+
+    if( !_nh.getParam("eps_yaw", _eps_yaw)) {
+        _eps_yaw = 0.001;
+    }
+
+    if( !_nh.getParam("K_ff_x_dot", _K_ff_x_dot)) {
+        _K_ff_x_dot = 1.0;
+    }
+
+    if( !_nh.getParam("vx_cruise", _vx_cruise)) {
+        _vx_cruise = 0.15;
+    }
+
+    if( !_nh.getParam("vx_max", _vx_max)) {
+        _vx_max = 0.2;
+    }
+
+    if( !_nh.getParam("vyaw_max", _vyaw_max)) {
+        _vyaw_max = 0.1;
+    }
+
+
 	_target_pub = _nh.advertise<mavros_msgs::PositionTarget>("/mavros/setpoint_raw/local", 1);
 
     _localization_sub   = _nh.subscribe( "/mavros/local_position/pose", 1, &SIMPLE_CLIENT::localization_cb, this);
@@ -28,6 +86,18 @@ SIMPLE_CLIENT::SIMPLE_CLIENT() {
     _joy_data_sub       = _nh.subscribe("/joy", 1, &SIMPLE_CLIENT::joy_cb, this);
     _wrench_sub         = _nh.subscribe("/NDT/Sensor_wrench", 1, &SIMPLE_CLIENT::sensorWrench_cb, this);           
     _desWrench_pub      = _nh.advertise<geometry_msgs::WrenchStamped>("/NDT/des_interaction_force", 1);
+
+
+    _range_l_sub = _nh.subscribe("/NDT/tfluna_left", 1, &SIMPLE_CLIENT::range_l_cb, this);
+    _range_r_sub = _nh.subscribe("/NDT/tfluna_right", 1, &SIMPLE_CLIENT::range_r_cb, this);
+    // _wall_pose_pub = _nh.advertise<geometry_msgs::PoseStamped>("/NDT/wall_pose", 1);
+    _wall_pose_filtered_pub = _nh.advertise<geometry_msgs::PoseStamped>("/NDT/wall_pose_filtered", 1);
+    _wall_xb_sp_pub = _nh.advertise<std_msgs::Float32>("/NDT/wall_dist_sp", 1);
+
+    const float samplingrate = 100.0; // Hz
+    const float cutoff_frequency = 2.0; // Hz
+    _butt_wall_x.setup (samplingrate, cutoff_frequency);
+    _butt_wall_yaw.setup(samplingrate, cutoff_frequency);
 
     _vel_joy << 0.0, 0.0, 0.0;
 
@@ -44,6 +114,9 @@ SIMPLE_CLIENT::SIMPLE_CLIENT() {
     _first_wrench = false;
     _currForce = 0.0;
     
+    _first_range_l = false;
+    _first_range_r = false;
+    _range_mount_dist = 0.42;
 }
 
 
@@ -55,31 +128,81 @@ void SIMPLE_CLIENT::mavros_state_cb( mavros_msgs::State mstate) {
 
 void SIMPLE_CLIENT::localization_cb ( geometry_msgs::PoseStampedConstPtr msg ) {
     _w_p << msg->pose.position.x, msg->pose.position.y, msg->pose.position.z;
-   
-    Eigen::Vector3d rpy = utilities::R2XYZ ( utilities::QuatToMat ( Eigen::Vector4d( msg->pose.orientation.w,  msg->pose.orientation.x,  msg->pose.orientation.y,  msg->pose.orientation.z) ) );
+    _w_q = Eigen::Vector4d( msg->pose.orientation.w,  msg->pose.orientation.x,  msg->pose.orientation.y,  msg->pose.orientation.z);
+    Eigen::Vector3d rpy = utilities::R2XYZ ( utilities::QuatToMat( _w_q ) );
     _mes_yaw = rpy(2);
 
-    Quaternionf q;
-    q = AngleAxisf(0.0, Vector3f::UnitX())
-        * AngleAxisf(0.0, Vector3f::UnitY())
-        * AngleAxisf(_mes_yaw, Vector3f::UnitZ());
-    Vector4d w_q ( q.w(), q.x(), q.y(), q.z() );
-    _w_q = w_q / w_q.norm() ;
+    // Quaternionf q;
+    // q = AngleAxisf(0.0, Vector3f::UnitX())
+    //     * AngleAxisf(0.0, Vector3f::UnitY())
+    //     * AngleAxisf(_mes_yaw, Vector3f::UnitZ());
+    // Vector4d w_q ( q.w(), q.x(), q.y(), q.z() );
+    // _w_q = w_q / w_q.norm() ;
 
-   
     _first_local_pos = true;    
 }
 
-void SIMPLE_CLIENT::position_controller(){
+void SIMPLE_CLIENT::range_l_cb(const sensor_msgs::RangeConstPtr &range_left_msg){
+    _range_l_val = range_left_msg->range;
+    _first_range_l = true;
+}
+
+void SIMPLE_CLIENT::range_r_cb(const sensor_msgs::RangeConstPtr &range_right_msg){
+    _range_r_val = range_right_msg->range;
+    _first_range_r = true;
+}
+
+void SIMPLE_CLIENT::joy_cb( sensor_msgs::JoyConstPtr j ) {
+
+    // LOGITECH F710 //Switch on D(DirectInput) and Mode led OFF
+    if( j->buttons[1] == 1 ) _enable_joy = true;
+    if( j->buttons[0] == 1 ) _enable_openarm = true;
+    if( j->buttons[2] == 1 ) _enable_closearm = true;
+    if( j->buttons[3] == 1 ) _enable_admittance = true;
+    if( j->buttons[9] == 1 ) _enable_interaction = true;
+    if( j->buttons[5] == 1 ) _enable_pump = true;
+    if( j->buttons[8] == 1 ) _enable_home = true;
+    // if( j->buttons[4] == 1 ) _enable_wall_ctrl = true;
+    
+    // tocheck
+    _vel_joy[0] = joy_ax0*j->axes[3]*0.2;
+    _vel_joy[1] = joy_ax1*j->axes[2]*0.2;
+    _vel_joy[2] = joy_ax2*j->axes[1]*0.2;
+    _vel_joy_dyaw = joy_ax3*j->axes[0]*0.2;
+    
+    // XBOX
+    // if( j->buttons[0] == 1 ) _enable_joy = true;
+    // if( j->buttons[2] == 1 ) _enable_openarm = true;
+    // if( j->buttons[1] == 1 ) _enable_closearm = true;
+    // if( j->buttons[3] == 1 ) _enable_admittance = true;
+    // if( j->buttons[9] == 1 ) _enable_interaction = true;
+    // if( j->buttons[5] == 1 ) _enable_pump = true;
+    // if( j->buttons[18] == 1 ) _enable_home = true;
+    
+    // _vel_joy[0] = joy_ax0*j->axes[4]*0.2;
+    // _vel_joy[1] = joy_ax1*j->axes[3]*0.2;
+    // _vel_joy[2] = joy_ax2*j->axes[1]*0.2;
+    // _vel_joy_dyaw = joy_ax3*j->axes[0]*0.2;
+
+}
+
+void SIMPLE_CLIENT::sensorWrench_cb(geometry_msgs::WrenchStamped msg){
+	_currForce = msg.wrench.force.z;
+	_first_wrench = true;
+}
+
+
+
+void SIMPLE_CLIENT::mavros_setpoint_publisher(){
 
     ros::Rate r(100);
 
     mavros_msgs::PositionTarget ptarget;
     ptarget.coordinate_frame = mavros_msgs::PositionTarget::FRAME_LOCAL_NED;
     ptarget.type_mask =
-    mavros_msgs::PositionTarget::IGNORE_VX |
-    mavros_msgs::PositionTarget::IGNORE_VY |
-    mavros_msgs::PositionTarget::IGNORE_VZ |
+    // mavros_msgs::PositionTarget::IGNORE_VX |
+    // mavros_msgs::PositionTarget::IGNORE_VY |
+    // mavros_msgs::PositionTarget::IGNORE_VZ |
     mavros_msgs::PositionTarget::IGNORE_AFX |
     mavros_msgs::PositionTarget::IGNORE_AFY |
     mavros_msgs::PositionTarget::IGNORE_AFZ |
@@ -89,21 +212,34 @@ void SIMPLE_CLIENT::position_controller(){
         usleep(0.1*1e6);
     ROS_INFO("First local pose arrived!");
 
+    
+
     _cmd_p = _w_p;
     _cmd_yaw = _mes_yaw;
+    _cmd_v[0] = 0.00;
+    _cmd_v[1] = 0.00;
+    _cmd_v[2] = 0.00;
+    _cmd_dyaw = 0.00;
 
     while (ros::ok()) {
-        if( _mstate.mode != "OFFBOARD" ) {
+        if( _mstate.mode != "OFFBOARD" ) { // No control: follow localization
             _cmd_p = _w_p;
             _cmd_yaw = _mes_yaw;
-        } // No control: follow localization
-
+            _cmd_v[0] = 0.00;
+            _cmd_v[1] = 0.00;
+            _cmd_v[2] = 0.00;
+            _cmd_dyaw = 0.00;
+        } 
         //---Publish command
         ptarget.header.stamp = ros::Time::now();
         ptarget.position.x = _cmd_p[0];
         ptarget.position.y = _cmd_p[1];
         ptarget.position.z = _cmd_p[2];
         ptarget.yaw = _cmd_yaw;
+        ptarget.velocity.x = _cmd_v[0];
+        ptarget.velocity.y = _cmd_v[1];
+        ptarget.velocity.z = _cmd_v[2];
+        ptarget.yaw_rate = _cmd_dyaw;
         _target_pub.publish( ptarget );
         //---
 
@@ -112,65 +248,133 @@ void SIMPLE_CLIENT::position_controller(){
 }
 
 
-void SIMPLE_CLIENT::joy_cb( sensor_msgs::JoyConstPtr j ) {
-
-    // LOGITECH F710
-    // Switch on D(DirectInput) and Mode led OFF
-    if( j->buttons[1] == 1 ) _enable_joy = true;
-    if( j->buttons[0] == 1 ) _enable_openarm = true;
-    if( j->buttons[2] == 1 ) _enable_closearm = true;
-    if( j->buttons[3] == 1 ) _enable_admittance = true;
-    if( j->buttons[9] == 1 ) _enable_interaction = true;
-    if( j->buttons[5] == 1 ) _enable_pump = true;
-    if( j->buttons[8] == 1 ) _enable_home = true;
-    
-    // tocheck
-    _vel_joy[0] = joy_ax0*j->axes[1]*0.2;
-    _vel_joy[1] = joy_ax1*j->axes[0]*0.2;
-    _vel_joy[2] = joy_ax2*j->axes[3]*0.2;
-    _vel_joy_dyaw = joy_ax3*j->axes[2]*0.2;
-    
-    // STEELSERIES
-    /*if( j->buttons[0] == 1 ) _enable_joy = true;
-    if( j->buttons[2] == 1 ) _enable_openarm = true;
-    if( j->buttons[1] == 1 ) _enable_closearm = true;
-    if( j->buttons[3] == 1 ) _enable_admittance = true;
-    if( j->buttons[9] == 1 ) _enable_interaction = true;
-    if( j->buttons[5] == 1 ) _enable_pump = true;
-    if( j->buttons[18] == 1 ) _enable_home = true;
-    
-    _vel_joy[0] = joy_ax0*j->axes[1]*0.2;
-    _vel_joy[1] = joy_ax1*j->axes[0]*0.2;
-    _vel_joy[2] = joy_ax2*j->axes[5]*0.2;
-    _vel_joy_dyaw = joy_ax3*j->axes[2]*0.2;*/
-
-}
-
-
-
-void SIMPLE_CLIENT::joy_ctrl () {
+void SIMPLE_CLIENT::joy_ctrl () { //diventa ibrido, controllo da joy su y e z, ma setpoint di distanza dal muro con x e yaw
 
     ros::Rate r(100);
-    
-    _joy_ctrl_active = true;
-    cout << "Activating joy control" << endl;
-    ROS_INFO("Activating joy control");
-    
-    while ( ros::ok() && _joy_ctrl ) {
 
-        if( _mstate.mode == "OFFBOARD" ) {
-            _cmd_p[0] += _vel_joy[0]*(1/100.0);
-            _cmd_p[1] += _vel_joy[1]*(1/100.0);
-            _cmd_p[2] += _vel_joy[2]*(1/100.0);
-            _cmd_yaw += _vel_joy_dyaw*(1/100.0);
+    while( !(_first_range_l && _first_range_r))
+        usleep(0.1*1e6);
+    ROS_INFO("First range l r arrived!");
+
+    
+    _joy_ctrl_active = false;
+    ROS_INFO("wall control loop active");
+    // geometry_msgs::PoseStamped wall_pose; //TODO debug only
+    geometry_msgs::PoseStamped wall_pose_filtered; //TODO debug only
+    std_msgs::Float32 wall_x_sp;
+    wall_pose_filtered.header.frame_id = "base_link";
+    wall_pose_filtered.pose.position.z = -0.30;
+    wall_pose_filtered.pose.position.y = 0.00;
+    // wall_pose_filtered = wall_pose; //TODO debug only
+    double wall_xb_raw =0.00;
+    double wall_yaw_raw =0.00;
+    // double _wall_xb =0.00;
+    // double _wall_yaw =0.00;
+    double e_x = 0.00;
+    double e_yaw = 0.00;
+    double e_x_old = 0.00;
+    double e_yaw_old = 0.00;
+    // double Kp_x = 0.2;
+    // double Kp_yaw = 0.3;
+    // double eps_x = 0.001;
+    // double eps_yaw = 0.001;
+    Eigen::Vector3d u_b;
+    Eigen::Vector3d u_w;
+    double u_dyaw;
+    Eigen::Matrix3d R_b_w;
+
+    _wall_xb_sp = (_range_l_val + _range_r_val) / 2.00;
+    _wall_xb_dot_sp = 0.00;
+    _green_light_for_ctrl = false;
+
+    while ( ros::ok() ) {
+        //compute wall dist and yaw
+        wall_xb_raw= (_range_l_val + _range_r_val) / 2.00; //TODO can be local to th
+        wall_yaw_raw = atan((_range_r_val - _range_l_val) / _range_mount_dist);
+        // Eigen::Vector4d wall_q_raw = utilities::rot2quat(utilities::XYZ2R(Eigen::Vector3d(0.0,0.0,_wall_yaw_raw))); //TODO debug only
+        _wall_xb = _butt_wall_x.filter(wall_xb_raw);
+        _wall_yaw = _butt_wall_yaw.filter( wall_yaw_raw);
+        Eigen::Vector4d wall_q = utilities::rot2quat(utilities::XYZ2R(Eigen::Vector3d(0.0,0.0,_wall_yaw)));
+        
+        if(fabs(_wall_yaw) <_wall_max_yaw && fabs(_wall_xb) <_wall_max_dist) _green_light_for_ctrl = true;
+        else _green_light_for_ctrl = false;
+        
+        if( _joy_ctrl && _green_light_for_ctrl) { //compute control only if drone is in offboard
+            
+            e_x = _wall_xb_sp - _wall_xb;
+            if(fabs(e_x) < _eps_x) e_x = 0.00;
+            e_yaw = -_wall_yaw;
+            if(fabs(e_yaw) < _eps_yaw) e_yaw = 0.00;
+
+            u_b << -(_Kp_x*e_x) -(_K_ff_x_dot*_wall_xb_dot_sp) ,_vel_joy[1], _vel_joy[2]; //wall ctrl + joy ctl on y_b and z_b
+            u_dyaw = -(_Kp_yaw*e_yaw );
+
+            //saturate velocity output
+            if(u_b(0) > _vx_max) u_b(0) =_vx_max;  
+            else if(u_b(0) < -_vx_max) u_b(0) = -_vx_max;  
+
+            if(u_b(1) > _vx_max) u_b(1) =_vx_max;  
+            else if(u_b(1) < -_vx_max) u_b(1) = -_vx_max;  
+
+            if(u_b(2) > _vx_max) u_b(2) =_vx_max;  
+            else if(u_b(2) < -_vx_max) u_b(2) = -_vx_max;  
+
+            if(u_dyaw > _vyaw_max) u_dyaw =_vyaw_max;  
+            else if(u_dyaw < -_vyaw_max) u_dyaw = -_vyaw_max;  
+
+            //rotate in global frame
+            R_b_w = utilities::QuatToMat(_w_q);
+            u_w = R_b_w*u_b;
+
+            // e_x_old =e_x;
+            // e_yaw_old =e_yaw;
+            
+            _cmd_p[0] += u_w[0]*(1/100.0);
+            _cmd_p[1] += u_w[1]*(1/100.0);
+            _cmd_p[2] += u_w[2]*(1/100.0);
+            _cmd_yaw += u_dyaw*(1/100.0);
+
+            _cmd_v[0] = u_w[0]; 
+            _cmd_v[1] = u_w[1];
+            _cmd_v[2] = u_w[2];
+            _cmd_dyaw = u_dyaw;
+
+            _joy_ctrl_active = true;
         }
+        else {  //if not in offboard reset integrals and outputs and overwrite sp 
+            _wall_xb_sp = _wall_xb; 
+            _wall_xb_dot_sp =0.00;
+            _cmd_p = _w_p;
+            _cmd_yaw = _mes_yaw;
+            _cmd_v[0] = 0.00;
+            _cmd_v[1] = 0.00;
+            _cmd_v[2] = 0.00;
+            _cmd_dyaw = 0.00;
+            _joy_ctrl_active = false;
+        } // No control: follow localization
+        
+        //publish wall pose (raw and filter, for debug)
+        // wall_pose.pose.position.x = _wall_xb_raw; //TODO debug only
+        // wall_pose.pose.orientation.w = wall_q_raw(0);
+        // wall_pose.pose.orientation.x = wall_q_raw(1);
+        // wall_pose.pose.orientation.y = wall_q_raw(2);
+        // wall_pose.pose.orientation.z = wall_q_raw(3);
+        // _wall_pose_pub.publish(wall_pose); //TODO debug only
 
-        _joy_ctrl_active = true;
+        wall_pose_filtered.pose.position.x = _wall_xb; //TODO debug only
+        wall_pose_filtered.pose.orientation.w = wall_q(0);
+        wall_pose_filtered.pose.orientation.x = wall_q(1);
+        wall_pose_filtered.pose.orientation.y = wall_q(2);
+        wall_pose_filtered.pose.orientation.z = wall_q(3);
+        wall_x_sp.data = _wall_xb_sp;
+        _wall_pose_filtered_pub.publish(wall_pose_filtered); //TODO debug only
+        _wall_xb_sp_pub.publish(wall_x_sp);
+
+        // _joy_ctrl_active = true;
         r.sleep();
     }
 
-    cout << "Deactivating joy control" << endl;
-    ROS_INFO("Deactivating joy control");
+    ROS_INFO("stop joy wall control loop");
     _joy_ctrl_active = false; 
 }
 
@@ -207,16 +411,51 @@ void SIMPLE_CLIENT::forceSetpointRise(double riseTime, double desX) {
 	}
 }
 
-void SIMPLE_CLIENT::sensorWrench_cb(geometry_msgs::WrenchStamped msg){
-	_currForce = msg.wrench.force.z;
-	_first_wrench = true;
-}
 
+
+void SIMPLE_CLIENT::traj_compute_scalar(double p_i, double p_f){
+    ros::Rate rate(100.0);
+    double dt = 1.0/100.0;
+    Eigen::Matrix<double,6,6> A;
+    double e = p_f-p_i;
+    double T = fabs(e)/_vx_cruise;
+    ROS_INFO("new traj, T=%f", T);
+    double s_f = fabs(e); //arclength;
+    Eigen::VectorXd b(6);
+    b << 0.0, 0.0, 0.0, s_f, 0.0, 0.0; //qi qi_d qi_dd qf qf_d qf_dd : initial final pos vel acc
+    A << 0,           0,           0,          0,        0,  1,
+         0,           0,           0,          0,        1,  0,
+         0,           0,           0,          1,        0,  0,
+         pow(T,5),    pow(T,4),    pow(T,3),   pow(T,2), T,  1,
+         5*pow(T,4),  4*pow(T,3),  3*pow(T,2), 2*T,      1,  0,
+         20*pow(T,3), 12*pow(T,2), 6*T,        1,        0,  0;
+ 
+    Eigen::VectorXd x = A.inverse()*b;
+    double s, s_d, s_dd;
+    double t = 0.0;
+    int N = T/dt;
+    int i =0;
+    double t_0 =ros::Time::now().toSec();
+    ROS_INFO("new traj, N=%d", N);
+    while(ros::ok() && i <=N){
+        //t = ros::Time::now().toSec()-t_0;
+        s    = x(0)*pow(t,5)    +x(1)*pow(t,4)    +x(2)*pow(t,3)   +x(3)*pow(t,2) +x(4)*t +x(5);
+        s_d  = x(0)*5*pow(t,4)  +x(1)*4*pow(t,3)  +x(2)*3*pow(t,2) +x(3)*2*t      +x(4);
+        s_dd = x(0)*20*pow(t,3) +x(1)*12*pow(t,2) +x(2)*6*t        +x(3);
+        
+        _wall_xb_sp = p_i + s*e/s_f;
+        _wall_xb_dot_sp = s_d*e/s_f; //not used, just position //TODO use ff ???
+        // _a = s_dd*e/s_f;
+        i++;
+        t+=dt;
+        rate.sleep();
+    }   
+}
 
 void SIMPLE_CLIENT::main_loop () {
 
     int enable_joy_cnt = 0;
-
+    // int enable_wall_ctrl_cnt = 0;
     int enable_openarm_cnt = 0;
     int enable_closearm_cnt = 0;
     int enable_admittance_cnt = 0;
@@ -237,7 +476,7 @@ void SIMPLE_CLIENT::main_loop () {
   	ros::ServiceClient disable_admittance_srv  = _nh.serviceClient<ndt_core_interface::enable_admittance>("/NDT/enable_admittance_mode");
   	ros::ServiceClient enable_interaction_srv  = _nh.serviceClient<ndt_core_interface::interaction_mode>("/NDT/enable_interaction_mode");
   	ros::ServiceClient disable_interaction_srv = _nh.serviceClient<ndt_core_interface::interaction_mode>("/NDT/enable_interaction_mode");
-    ros::ServiceClient home_arm_srv                = _nh.serviceClient<ndt_core_interface::close>("/NDT/home_arm");
+    ros::ServiceClient home_arm_srv            = _nh.serviceClient<ndt_core_interface::close>("/NDT/home_arm");
 	ros::Publisher gelPump_pub = _nh.advertise<std_msgs::Int32>("/NDT/pump_time", 1);
     
 	std_msgs::Int32 pumpTime;
@@ -270,12 +509,20 @@ void SIMPLE_CLIENT::main_loop () {
         enable_admittance_cnt++;
         enable_interaction_cnt++;
         enable_home_cnt++;
+        // enable_wall_ctrl_cnt++;
 
-        if( _enable_joy == true && enable_joy_cnt > 50) {
+        // if( _enable_wall_ctrl == true && enable_wall_ctrl_cnt > 50) {
+        //     _wall_ctrl = !_wall_ctrl;
+        //     enable_wall_ctrl_cnt = 0;
+        //     _enable_wall_ctrl = false;
+        // }
+
+        if( _enable_joy == true && enable_joy_cnt > 50) { //now is hybrid joy + wall control
             _joy_ctrl = !_joy_ctrl;
             enable_joy_cnt = 0;
             _enable_joy = false;
         }
+
 
         if( _enable_openarm && enable_openarm_cnt > 50) {
             enable_openarm_cnt = 0;
@@ -361,12 +608,23 @@ void SIMPLE_CLIENT::main_loop () {
 
         //Enable disable joy ctrl
         if( _joy_ctrl && !_joy_ctrl_active ) {     
-            boost::thread joy_ctrl_t (&SIMPLE_CLIENT::joy_ctrl, this);
-            usleep(0.2*1e6);
+            // boost::thread joy_ctrl_t (&SIMPLE_CLIENT::joy_ctrl, this);
+            if(_green_light_for_ctrl){
+                ROS_INFO("Green Light, Activating joy/wall control");
+                r.sleep();
+                ROS_INFO("going to d_safe");
+                traj_compute_scalar(_wall_xb_sp,_d_safe);
+            } 
+            else{
+                ROS_WARN("out of range for activating control");
+                _joy_ctrl = false;
+            }
+            // usleep(0.2*1e6);
         }
         else if (!_joy_ctrl ) {
             if (_joy_ctrl_active == true ) {
                 _joy_ctrl_active = false;
+                ROS_INFO("Deactivating joy/wall control");
             }
         }
 
@@ -375,7 +633,8 @@ void SIMPLE_CLIENT::main_loop () {
 }
 
 void SIMPLE_CLIENT::run(){
-    boost::thread position_controller_t( &SIMPLE_CLIENT::position_controller, this);
+    boost::thread mavros_setpoint_publisher_t( &SIMPLE_CLIENT::mavros_setpoint_publisher, this);
+    boost::thread joy_ctrl_t (&SIMPLE_CLIENT::joy_ctrl, this);
     boost::thread main_loop_t( &SIMPLE_CLIENT::main_loop, this );
     ros::spin();
 }
